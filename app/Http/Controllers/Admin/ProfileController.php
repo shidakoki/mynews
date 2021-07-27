eaa35c4a4647ff39fe3ab3e498644c7b7539d0fc
shidakoki <kouki_c@icloud.com>
2021-07-27 13:56:32 +0000

    seven

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,50 +5,45 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class ProfileController extends Controller
+// 以下を追記することでNews Modelが扱えるようになる
+use App\News;
+​
+class NewsController extends Controller
 {
   public function add()
   {
-    return view('admin.profile.create');
+      return view('admin.profile.create');
   }
-  
-  public function create()
+​
+  public function create(Request $request)
   {
-    return redirect('admin/profile/create');
-  }
-  
-  public function edit()
-  {
-    return view('admin.profile.edit');
-  }
-  
-  public function update()
-  {
-    return redirect('admin/profile/edit');
+​
+      // 以下を追記
+      // Varidationを行う
+      $this->validate($request, News::$rules);
+​
+      $news = new News;
+      $form = $request->all();
+​
+      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+​
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+​
+      // データベースに保存する
+      $news->fill($form);
+      $news->save();
+​
+      return redirect('admin/profile/create');
   }
 }
 
-/*
-PHP/Laravel　08　ControllerとRoutingの関係について理解しよう
-
-課題2
-Routingの役割
-　特定のアドレスにアクセスしたとき、どの処理を呼び出して実行するか。
-　それを管理するのがRoutingである。
-Controllerの役割
-　Modelの連携をとってデータベースからデータを取得したり、データを保存したりする。
-　その後、Modelから取得したデータをViewと連携してブラウザに表示するデータを生成し、
-　Viewにデータを送信するように指示を送る。
-
-PHP/Laravel　09　Routingについて理解する
-課題1　Routing
-課題2　group化することでRoutingの設定を複数行うことが可能にうなるから
-課題3　
-Route::get('/XXX',
-'AAAController@bbb');
-
-PHP/Laravel 10 ControllerとViewが連携できるようにしよう
-課題1　Controllerの指示によってアクセスしてきたユーザーに表示するデータを生成する
-課題2　プログラマーが初めからHTMLを書くと決まったものを表示するHTMLしか出力できない。
-課題3　Viewsのadminのprofileにcreate.blade.phpとedit.blade.phpファイルを設置する。
-*/
+?>
