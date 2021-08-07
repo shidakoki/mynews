66a5a7a063a9e40242b776e8e69d7e8dfbddcd6a
shidakoki <kouki_c@icloud.com>
2021-08-07 12:49:51 +0000

    16

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::group(['prefix' => 'admin'], function() {
   Route::get('news/create','Admin\NewsController@add')->middleware('auth');
   Route::post('news/create', 'Admin\NewsController@create');
   Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+  Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+  Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+  Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
 
 Route::get('admin/profile/create','Admin\ProfileController@add')->middleware('auth');
