616f2a71e8c034bd7eb9cf1410aa4a2a6f643dac
shidakoki <kouki_c@icloud.com>
2021-08-17 04:42:05 +0000

    zzz

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,5 @@ Route::get('XXX',
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'NewsController@index');
+Route::get('//', 'ProfileController@index');
